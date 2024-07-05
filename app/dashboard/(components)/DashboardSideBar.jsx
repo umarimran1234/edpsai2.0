3161aa31c1a0b8ab2196d750fb9470e5c1c5a42a
umarimran1234 <umartkd989@gmail.com>
2024-07-04 17:31:12 -0700

    this is new

--- app/dashboard/(components)/DashboardSideBar.jsx
+++ app/dashboard/(components)/DashboardSideBar.jsx
@@ -6,7 +6,7 @@ import { ClockIcon, GlobeIcon , LightningBoltIcon } from '@radix-ui/react-icons'
 // import { LightBulbIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Bars3Icon } from '@heroicons/react/24/outline';
-
+import {CubeTransparentIcon} from '@heroicons/react/24/outline';
 function DashboardSideBar() {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -17,21 +17,21 @@ function DashboardSideBar() {
   return (
     <div className="flex " >
       {/* Sidebar */}
-      <div className={`fixed inset-y-0 left-0 z-30  transform bg-gray-800 p-2 transition-transform duration-300 ease-in-out ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0`}>
-        <div className="flex items-center  justify-between mb-6">
+      <div className={`fixed inset-y-0 left-0 z-30  transform  p-2 transition-transform duration-300 ease-in-out ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0`}>
+        <div className="flex items-center mt-3 px-5 justify-between mb-6">
             
-            <Bars3Icon/>
-
           <button onClick={toggleSidebar} className=" text-white">
+            <Bars3Icon color='white' width={20} />
+
 
           </button>
         </div>
  <nav defaultActiveKey="/home" className="flex-column bg-dark text-white" style={{ width: '60px', height: '100vh'  }}>
-      <ul  style={ { padding:'1.4rem',  borderRadius:'2rem' ,   background:'#3A3A3C'}} >
+      <ul  style={ { padding:'1.4rem',  borderRadius:'1rem' ,   background:'#3A3A3C'}} >
    <li className='mb-4' >
 
       <Link href="/link-2" className="text-center text-white">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4">
+        <svg style={{color:'#898181'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4">
   <path fillRule="evenodd" d="M2.75 9a.75.75 0 0 1 .75.75v1.69l2.22-2.22a.75.75 0 0 1 1.06 1.06L4.56 12.5h1.69a.75.75 0 0 1 0 1.5h-3.5a.75.75 0 0 1-.75-.75v-3.5A.75.75 0 0 1 2.75 9ZM2.75 7a.75.75 0 0 0 .75-.75V4.56l2.22 2.22a.75.75 0 0 0 1.06-1.06L4.56 3.5h1.69a.75.75 0 0 0 0-1.5h-3.5a.75.75 0 0 0-.75.75v3.5c0 .414.336.75.75.75ZM13.25 9a.75.75 0 0 0-.75.75v1.69l-2.22-2.22a.75.75 0 1 0-1.06 1.06l2.22 2.22H9.75a.75.75 0 0 0 0 1.5h3.5a.75.75 0 0 0 .75-.75v-3.5a.75.75 0 0 0-.75-.75ZM13.25 7a.75.75 0 0 1-.75-.75V4.56l-2.22 2.22a.75.75 0 1 1-1.06-1.06l2.22-2.22H9.75a.75.75 0 0 1 0-1.5h3.5a.75.75 0 0 1 .75.75v3.5a.75.75 0 0 1-.75.75Z" clipRule="evenodd" />
 </svg>
 
@@ -41,27 +41,40 @@ function DashboardSideBar() {
 <li className='mb-4'>
 
       <Link href="/home" className="text-center text-white">
-        <ClockIcon />
+        <ClockIcon color='#898181' />
       </Link>
 </li>
     
    <li className='mb-4' >
   <Link href="/link-1" className="text-center text-white">
-        <GlobeIcon />
+        <GlobeIcon color='#898181' />
       </Link>
    </li> 
    <li className='mb-4' >
 
       <Link href="/link-2" className="text-center text-white">
-        <LightningBoltIcon />
+        <LightningBoltIcon color='#898181' />
       </Link>
-   </li>
 
+   </li>
+<li className='mb-4' >
+
+      <Link href="/link-2" className="text-center text-white">
+        <CubeTransparentIcon color='#898181' />
+      </Link>
+      
+   </li>
       </ul>
 
     </nav>
       </div>
-
+<div className="flex-1 p-6 md:ml-64 absolute block lg:hidden">
+        <button onClick={toggleSidebar} className="md:hidden mb-4 text-gray-700">
+          <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
+          </svg>
+        </button>
+        </div>
       {/* Main Content */}
      
     </div>
--- app/dashboard/(components)/DashboardTopNav.jsx
+++ app/dashboard/(components)/DashboardTopNav.jsx
@@ -30,8 +30,8 @@ function DashboardTopNav() {
   return (
     <>
       <div className="min-h-full">
-        <Disclosure as="nav" className="bg-gray-800">
-          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <Disclosure as="nav" >
+          <div style={{borderBottom:'1px white '}} className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="flex h-16 items-center justify-between">
               <div className="flex items-center">
                 <div className="flex-shrink-0">
@@ -52,7 +52,7 @@ function DashboardTopNav() {
                         {item.name}
                       </a>
                     ))}
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <svg style={{color:'white'}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                      
                        <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
 
@@ -62,13 +62,15 @@ function DashboardTopNav() {
               </div>
               <div className="hidden md:block">
                 <div className="ml-4 flex items-center md:ml-6">
+                  <h5>Bassie Cooper</h5>
+                  
                   <button
                     type="button"
                     className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                   >
                     <span className="absolute -inset-1.5" />
                     <span className="sr-only">View notifications</span>
-                    <BellIcon aria-hidden="true" className="h-6 w-6" />
+                    {/* <BellIcon aria-hidden="true" className="h-6 w-6" /> */}
                   </button>
 
                   {/* Profile dropdown */}
