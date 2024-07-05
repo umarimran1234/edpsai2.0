62781367d677e7650ee7041c409d6a4bbfe27a21
umarimran1234 <umartkd989@gmail.com>
2024-07-05 05:26:06 -0700

    this is updte

--- app/dashboard/(components)/Leftsidebar.jsx
+++ app/dashboard/(components)/Leftsidebar.jsx
@@ -1,30 +1,36 @@
 // components/Sidebar.js
+import Image from 'next/image';
 import React from 'react';
+import contineuwork from '../../../public/Images/dashboard/Problem Description.png'
+import contributor1 from '../../../public/Images/dashboard/Rectangle 32 (1).png'
+import contributor2 from '../../../public/Images/dashboard/Rectangle 32 (2).png'
+import contributor3 from '../../../public/Images/dashboard/Rectangle 32 (3).png'
+import contributor4 from '../../../public/Images/dashboard/Rectangle 32.png'
 
 const contributors = [
   {
     role: 'Data Analyst',
     name: 'Micole Jackson',
     email: 'micol3jac@email.com',
-    image: 'https://via.placeholder.com/50',
+    image: contributor1,
   },
   {
     role: 'App Developer',
     name: 'Diplor Lore',
     email: 'diplore@email.com',
-    image: 'https://via.placeholder.com/50',
+    image: contributor2,
   },
   {
     role: 'UX Designer',
     name: 'Drake Josh',
     email: 'whyth3snake@email.com',
-    image: 'https://via.placeholder.com/50',
+    image: contributor3
   },
   {
     role: 'Backend Developer',
     name: 'Olivio Rodriguez',
     email: 'maybeasinger@email.com',
-    image: 'https://via.placeholder.com/50',
+    image: contributor4,
   },
 ];
 
@@ -36,8 +42,8 @@ const Sidebar = () => {
     <div className="marl bg-black col-span-1 text-white h-full w-80 p-6  right-0 top-0 flex flex-col space-y-6">
       <div>
         <h2 className="text-xl font-semibold mb-4">Continue Working</h2>
-        <img
-          src="https://via.placeholder.com/300x150"
+        <Image 
+          src={contineuwork}
           alt="Working"
           className="mb-4"
         />
@@ -47,7 +53,7 @@ const Sidebar = () => {
         <h2 className="text-xl font-semibold mb-4">Contributors</h2>
         {contributors.map((contributor, index) => (
           <div key={index} className="flex items-center space-x-4 mb-4">
-            <img
+            <Image
               src={contributor.image}
               alt={contributor.name}
               className="w-12 h-12 rounded-full"
--- app/dashboard/Problems/page.jsx
+++ app/dashboard/Problems/page.jsx
@@ -4,7 +4,7 @@ import { BreadcrumbDemo } from "../../../components/BreadCrums"
 import Card from "../../../components/Gridcard"
 import { Input } from "../../../components/ui/input"
 import coffie from "../../../public/Images/dashboard/Rectangle 33.png" 
-
+import { PlusIcon } from "@radix-ui/react-icons"
 export default function problempage() {
     return(
         
@@ -12,14 +12,14 @@ export default function problempage() {
             <BreadcrumbDemo hreffirst='/' hrefthird='#' hrefsecond='/dashboard' condition={true} firstpath='Company' secondpath='Deshboard' thirdpath='Problems'  />
             <div className="mt-12">
 
-        <Input className='borderdark' disable value='Welcome back  Bassie Copper'/>       
+           
             </div>
             
             
      <div className="min-h-screen  py-8">
       <div className="mb-5">
 
-      <h5 style={{fontWeight:'bold'}} >Recent Project</h5>
+      <h5 style={{fontWeight:'bold'}} > Projects</h5>
       </div>
       
       <div className="container mx-auto px-4">
@@ -27,12 +27,13 @@ export default function problempage() {
       <div className="grid grid-cols-1 md:grid-cols-1 w-50">
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <Card imageSrc={coffie} heading="Circuit Burnout " text="This is the first card." />
+          <Card  imageSrc={coffie}  heading="Circuit Burnout " text="This is the first card." />
           <Card imageSrc={coffie} heading="Motor Squealing" text="This is the second card." />
           <Card imageSrc={coffie} heading="The Shredding" text="This is the third card." />
           <Card imageSrc={coffie} heading="Coffiee machine leaking" text="This is the fourth card." />
         </div>
       </div>
+       <button style={{background:'#888888',padding:'1rem',  borderRadius:"1rem" } } className="mt-6"> <PlusIcon width={50} fontSize={100} /> </button>
       </div>
     </div>
             
